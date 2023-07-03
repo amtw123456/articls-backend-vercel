56e007f0b362d635c5925bbd65a0930b0bb43bb8
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2023-07-04 07:51:41 +0800

    fix: allowed all

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8+r-lf5kwly#i2k!v4e$n*@c56smhjr&7+m9tsfet686!^xik7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 

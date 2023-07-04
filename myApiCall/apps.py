5a8a753cb033ff7977e9fce4edc5061ba299deec
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2023-07-04 08:53:14 +0800

    redasdqdasdqwwdasdqw

--- myApiCall/apps.py
+++ myApiCall/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class AppConfig(AppConfig):
+class MyApiCallsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myApiCall'
 
